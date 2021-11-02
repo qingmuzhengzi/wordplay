8d7eb02457077df55708a0059ec6652868242f8d
unknown <qingmuzhengzi@126.com>
2021-11-02 21:37:23 +0800

    wordplay

---
+++ 文字游戏/文字游戏/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 文字游戏
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("12点了，……");
+            Console.ReadLine();
+            Console.WriteLine("依旧守在电脑面前。\n 他写下了一个名字，……");
+            Console.ReadLine();
+            Console.WriteLine("出现了。\n 他说：他会帮你杀了他。\n 但是你要把……");
+            Console.ReadLine();
+            Console.WriteLine("给他\n 你同意了。");
+            Console.ReadKey();
+        }
+    }
+}
